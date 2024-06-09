72b49fd402e32ff9c99e7361df40d18099a62b46
samskrutha <samskruthams@gmail.com>
2024-06-09 21:16:29 +0200

    update the code and added .gitignore file

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -5,18 +5,13 @@ resource "aws_eks_cluster" "eks_cluster" {
   vpc_config {
     subnet_ids = var.subnet_ids
   }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.eks_AmazonEKSServicePolicy,
-  ]
 }
 
 resource "aws_eks_node_group" "eks_nodes" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = var.node_group_name
   node_role_arn   = var.node_role_arn
-  subnets         = var.subnet_ids
+  subnet_ids      = var.subnet_ids
 
   scaling_config {
     desired_size = var.desired_capacity
